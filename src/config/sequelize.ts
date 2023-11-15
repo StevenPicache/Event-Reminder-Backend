3e411d213648cd459b8c0dbc6a9c9d91c4261b1c
Steven Picache <steven@zeitspace.com>
2023-11-15 00:07:59 -0500

    add week range search

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -1,39 +1,33 @@
-import { Sequelize } from "sequelize"
+import { Sequelize } from 'sequelize'
 import { config } from './config'
 
-const sequelize = new Sequelize(
-    config.database,
-    config.user,
-    config.password,
-    {
-      host: config.host,
-      dialect: 'postgres'
-    }
-)
+const sequelize = new Sequelize(config.database, config.user, config.password, {
+    host: config.host,
+    dialect: 'postgres',
+})
 
-async function testConnection(){
+async function testConnection() {
     try {
         await sequelize.authenticate()
-        console.log("Database connection has been established")
+        console.log('Database connection has been established')
     } catch (e) {
         console.log(e)
     }
 }
 
-async function testSyncTable(){
+async function testSyncTable() {
     try {
-        await sequelize.sync({alter: true})
-        console.log("Models synced/altered successfully")
+        await sequelize.sync({ alter: true })
+        console.log('Models synced/altered successfully')
     } catch (e) {
         console.log(e)
     }
 }
 
-export function SequelizeInit() { 
-    console.log("Initializing tables")
+export function SequelizeInit() {
+    console.log('Initializing tables')
     testConnection()
     testSyncTable()
 }
-
 
 export default sequelize
--- src/routes/events.ts
+++ src/routes/events.ts
@@ -1,15 +1,35 @@
-import express, { Express, Request, Response, Router } from 'express';
-import { EventServices, SearchText } from '../services/events';
+import express, { Express, Request, Response, Router } from 'express'
+import { EventServices, SearchText } from '../services/events'
 
 const router = express.Router()
-
 
 const getEvents = async (req: Request, res: Response) => {
     try {
         const result = await EventServices.getEvents()
-        res.status(200).send(result);
+        res.status(200).send(result)
     } catch (e: any) {
-        /// TODO / TECH DEBT: Handle error properly without using type any
+        console.error(e)
+        res.status(500).send(e.toString())
+    }
+}
+
+const getSearchEvents = async (req: Request, res: Response) => {
+    try {
+        const searchText = req.params.search
+        const result = await EventServices.searchEvents({ searchText })
+        res.status(200).send(result)
+    } catch (e: any) {
+        console.error(e)
+        res.status(500).send(e.toString())
+    }
+}
+
+const getEventWithRange = async (req: Request, res: Response) => {
+    try {
+        const range = parseInt(req.params.range)
+        const result = await EventServices.getEventsWeekRange({ range })
+        res.status(200).send(result)
+    } catch (e: any) {
         console.error(e)
         res.status(500).send(e.toString())
     }
@@ -18,30 +38,16 @@ const getEvents = async (req: Request, res: Response) => {
 const addEvent = async (req: Request, res: Response) => {
     try {
         const result = await EventServices.addEvent(req.body)
-        res.status(200).send(result);
+        res.status(200).send(result)
     } catch (e: any) {
-        /// TODO / TECH DEBT: Handle error properly without using type any
         console.error(e)
         res.status(500).send(e.toString())
     }
 }
 
-
-const getSearchEvents = async (req: Request, res: Response) => {
-    try {
-        const searchText = req.params.param
-        const result = await EventServices.searchEvents({searchText})
-        res.status(200).send(result);
-    } catch (e: any) {
-        /// TODO / TECH DEBT: Handle error properly without using type any
-        console.error(e)
-        res.status(500).send(e.toString())
-    }
-}
-
-
-router.get('/v0/events', getEvents);
-router.get('/v0/events/:param', getSearchEvents);
-router.post('/v0/events', addEvent);
+router.get('/v0/events', getEvents)
+router.get('/v0/events/:search', getSearchEvents)
+router.get('/v0/events/range/:range', getEventWithRange)
+router.post('/v0/events', addEvent)
 
 export default router
